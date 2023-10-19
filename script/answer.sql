7d76d5d6ce4ffc1ee0e4fc78072249c315aa3c3a
mhm516 <joycelynn.c.jacobs@gmail.com>
2023-10-19 12:01:54 -0500

    added the avg rating, but I think it is incorrect

--- script/answer.sql
+++ script/answer.sql
@@ -11,20 +11,26 @@
 
 select
 distinct app_store_apps.name,
-play_store_apps.rating,
-app_store_apps.rating,
-cast(app_store_apps.review_count as integer) as ios_review_integer, sum(play_store_apps.review_count)/2 as combined_avg_review_count,
+--did this to eliminate duplicate play store apps
+round(avg(play_store_apps.rating),2) as avg_p_rating,
+round(avg(app_store_apps.rating),2) as avg_a_rating,
+cast(app_store_apps.review_count as integer) as ios_review_integer,
+--did this to take this from text to integer
+sum(play_store_apps.review_count)/2 as combined_avg_review_count,
+--did this to combine all the similar play store review counts
 case
 			when cast(app_store_apps.price as money) > cast(play_store_apps.price as money)
 			 then  cast(app_store_apps.price as money)
 			 else  cast(play_store_apps.price as money)
 			 end as greater_price
+			 --did this to only display the highest app price between the two
 from app_store_apps
 inner join play_store_apps
 using (name)
 where play_store_apps.rating > 
 	(select avg(play_store_apps.rating) from play_store_apps)
 and app_store_apps.rating > (select avg(app_store_apps.rating) from app_store_apps)
-group by distinct app_store_apps.name, ios_review_integer, greater_price, play_store_apps.rating,
-app_store_apps.rating
-order by play_store_apps.rating, app_store_apps.rating desc;
+--did this to only display above avg apps, in regards to their rating
+group by distinct app_store_apps.name, ios_review_integer, greater_price
+order by avg_p_rating desc
+limit 20;
