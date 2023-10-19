0f8bae3e6d287bc8bcc60807cd7a2df5f104fe0e
mhm516 <joycelynn.c.jacobs@gmail.com>
2023-10-19 13:42:18 -0500

    fixed the ratings and added a review condition

--- script/answer.sql
+++ script/answer.sql
@@ -9,28 +9,32 @@
 --An app that is on both app stores will make $10,000 per month. 
 
 
-select
-distinct app_store_apps.name,
+SELECT
+DISTINCT app_store_apps.name,
 --did this to eliminate duplicate play store apps
-round(avg(play_store_apps.rating),2) as avg_p_rating,
-round(avg(app_store_apps.rating),2) as avg_a_rating,
-cast(app_store_apps.review_count as integer) as ios_review_integer,
+play_store_apps.rating AS android_rating,
+app_store_apps.rating AS ios_rating,
+CAST
+	(app_store_apps.review_count AS integer) AS ios_review_integer,
 --did this to take this from text to integer
-sum(play_store_apps.review_count)/2 as combined_avg_review_count,
+SUM(play_store_apps.review_count) AS android_review_count,
 --did this to combine all the similar play store review counts
-case
-			when cast(app_store_apps.price as money) > cast(play_store_apps.price as money)
-			 then  cast(app_store_apps.price as money)
-			 else  cast(play_store_apps.price as money)
-			 end as greater_price
-			 --did this to only display the highest app price between the two
-from app_store_apps
-inner join play_store_apps
-using (name)
-where play_store_apps.rating > 
-	(select avg(play_store_apps.rating) from play_store_apps)
-and app_store_apps.rating > (select avg(app_store_apps.rating) from app_store_apps)
---did this to only display above avg apps, in regards to their rating
-group by distinct app_store_apps.name, ios_review_integer, greater_price
-order by avg_p_rating desc
-limit 20;
+CASE
+			WHEN CAST(app_store_apps.price AS money) > CAST(play_store_apps.price AS money)
+			 THEN CAST(app_store_apps.price AS money)
+			 ELSE CAST(play_store_apps.price AS money)
+			 END AS greater_price
+			 --did this to only display the highest app price between              the two
+FROM app_store_apps
+INNER JOIN play_store_apps
+USING (name)
+WHERE play_store_apps.rating> 4.20
+--figured the avg using this (select round(avg(play_store_apps.rating),2) as avg_p_rating from play_store_apps)
+	AND app_store_apps.rating>3.54 
+--figured the avg using this (select round(avg(app_store_apps.rating),2) as avg_a_rating from app_store_apps)
+	AND play_store_apps.review_count>10000
+	AND CAST
+	(app_store_apps.review_count AS integer)>10000
+GROUP BY DISTINCT app_store_apps.name, play_store_apps.rating, app_store_apps.rating, ios_review_integer, greater_price
+ORDER BY play_store_apps.rating DESC
+LIMIT 10;
