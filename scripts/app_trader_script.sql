af740815aad278f7f30cbbd0e5ab1a56cd666226
brandylarramore <brandy.larramore@gmail.com>
2023-10-19 20:49:42 -0400

    still working on narrowing down top 10, trying to figure out how to compare top profit

--- scripts/app_trader_script.sql
+++ scripts/app_trader_script.sql
@@ -11,23 +11,61 @@ ORDER BY average_rating DESC;
 
 --Looking at which ones are free on both stores--
 
-SELECT ios.name AS iosname, android.name AS droidname, ios.price AS ios_price, android.price, CASE WHEN ios.price > android.price
-THEN ios.price 
-ELSE android.price
-END AS higer_price
-AS droid_price,
-  ROUND((ios.rating + android.rating) / 2, 2) AS average_rating
+SELECT DISTINCT ios.name AS iosname, droid.name AS droidname, ios.price AS ios_price, droid.price AS droid_price,
+  ROUND((ios.rating + droid.rating) / 2, 2) AS average_rating
 FROM app_store_apps AS ios
-INNER JOIN play_store_apps AS android
-ON ios.name = android.name
-WHERE ios.price = 0 AND android.price = '0'
+INNER JOIN play_store_apps AS droid
+ON ios.name = droid.nameWHERE (ios.price = 0 AND droid.price = '0')
+   OR (ios.price = 0.99 AND droid.price = '0.99')
+ORDER BY average_rating DESC;
+		
+		
 ORDER BY average_rating DESC;
 
 
---case code?--
-case
-		when cast(replace(p.price, '$', '') as money) > cast(a.price as money)
-		then cast(replace(p.price, '$', '') as money)
-		else  cast(a.price as money)
-	end as higher_price,
+
+find net profit and gross profit
+
+
+
+SELECT name, average_rating
+FROM app_store_apps AS ios
+
+
+
+
+
+
+
+
+
+
+
+--All apps from both stores priced 0-1--
+WITH CombinedApps AS (
+    SELECT name, 
+           CASE 
+               WHEN price = '0' THEN 0.0
+               WHEN price = '$0.99' THEN 0.99
+           END AS price
+    FROM play_store_apps
+    WHERE price = '0' OR price = '$0.99'
+
+    UNION
+
+    SELECT name, 
+           CASE 
+               WHEN price = 0 THEN 0.0
+               WHEN price = 0.99 THEN 0.99
+           END AS price
+    FROM app_store_apps
+    WHERE price = 0 OR price = 0.99
+)
+SELECT name, price
+FROM CombinedApps
+ORDER BY price ASC;
+
+
+
+
 
