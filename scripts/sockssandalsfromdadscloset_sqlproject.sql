e1b15f48e07eec83ab3616fcd75558288dd35097
jbarber <barberjordann@gmail.com>
2023-10-21 12:22:24 -0500

    finished project,query finished but not complete

--- scripts/sockssandalsfromdadscloset_sqlproject.sql
+++ scripts/sockssandalsfromdadscloset_sqlproject.sql
@@ -21,8 +21,9 @@ USING (name)
 ORDER BY avg_rating DESC;
 
 -- Average rating for apps in both store + the apps with the highest price --
-SELECT DISTINCT a.name,
-	((a.rating + p.rating)/2) AS avg_rating,
+with cte as (
+	SELECT DISTINCT p.name as app_name,
+	ROUND((a.rating + p.rating)/2,1) AS avg_rating,
 CASE WHEN cast(replace(p.price, '$', '') as money) > cast(a.price as money)
 		then cast(replace(p.price, '$', '') as money)
 		else  cast(a.price as money)
@@ -38,3 +39,48 @@ LIMIT 10;
 	
 	
 --Need a case statement for the price, subquery for multiple conditions for the WHERE ---
+
+WITH iso_cte AS
+(SELECT DISTINCT content_rating
+FROM app_store_apps
+ORDER BY CONTENT_RATING DESC),
+droid_cte AS
+(SELECT DISTINCT content_rating
+FROM play_store_apps
+ ORDER BY content_rating DESC),
+SELECT *
+from iso_cte
+FULL JOIN droid_cte
+USING (content_rating)
+
+
+
+SELECT DISTINCT CONTENT_RATING
+from app_store_apps
+order by content_rating
+   
+	
+with cte as (	
+	SELECT DISTINCT p.name as app_name,
+	ROUND((a.rating + p.rating)/2,1) AS avg_rating,
+CASE WHEN cast(replace(p.price, '$', '') as money) > cast(a.price as money)
+		then cast(replace(p.price, '$', '') as money)
+		else  cast(a.price as money)
+	end as higher_price
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+USING (name)),	
+-- 2ND PART OF QUERY--
+cte0 as(
+	SELECT
+    app_name,
+	avg_rating,
+	higher_price,
+	play_store_price,
+	app_store_price,
+CASE WHEN higher_price > 1.00::money
+then (higher_price::numeric * 10000)::money 
+	ELSE 1000::money 
+END AS purchase_price,
+floor((avg_rating/.5)+1) as longevity_in_years
+FROM ),
